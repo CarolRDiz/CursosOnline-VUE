75be5c04d77a0943c5d96e253d75fd6661f14f16
Carolina Rodríguez <crodriguez@iesrafaelalberti.es>
2023-03-10 23:33:06 +0100

    Delete dist directory

--- dist/assets/About-88cae7b1.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index-b3de40db.js";const t={};function c(e,_){return" ABOUT "}const o=r(t,[["render",c]]);export{o as default};
--- dist/assets/verificar-a2122b4b.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/verificar-48031578.png";export{s as _};
